781b93d656410dca43e47ed5502c03807a4a9e93
AidenOliver1 <94108662+AidenOliver1@users.noreply.github.com>
2022-10-14 16:32:12 +0800

    Add files via upload

---
+++ snowAuthenticationHelper.cs
@@ -0,0 +1,67 @@
+/*
+.SYNOPSIS
+    HelperFile for setting up authentication to the ServiceNow REST API ([Incident] table) and returning an OAuth2.0 AccessToken
+.PREREQUISITES
+    Install NUGET package RestSharp v108.0.2
+.DESCRIPTION
+
+    Creates callable public properties of the "snowAuthenticationHelper" class for use in the next method 
+    Creates a callable method called "getSnowAuthenticationToken" which sends a POST request to the "oauth_token.do" endpoint of the ServiceNow API
+
+.EXAMPLE
+    Call from Controller / Program using the following syntax:
+        var snowAuth = new snowAuthenticationHelper();
+        var snowRequest = new snowRequestHelper();
+        snowAuth.restClientUrl = snowRestClientUrl;
+        snowAuth.clientId = snowClientId;
+        snowAuth.clientSecret = snowClientSecret;
+        snowAuth.username = snowIntegratedUsername;
+        snowAuth.password = snowIntegratedPassword;
+        snowAuth.bearerAccessToken = snowAuth.getSnowAuthenticationToken();
+.OUTPUTS
+    Returns the ServiceNow OAuth2.0 AccessToken as a string
+.NOTES
+    Created on: 14.10.2022
+    Author:     Aiden Oliver
+*/
+
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace /*InsertNamespaceHere*/.snowHelperFiles
+{
+    internal class snowAuthenticationHelper
+    {
+        public string restClientUrl { get; set; }
+        public string clientId { get; set; }
+        public string clientSecret { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string bearerAccessToken { get; set; }
+
+        public string getSnowAuthenticationToken()
+        {
+            RestClient restClient = new RestClient(restClientUrl);
+            var request = new RestRequest("oauth_token.do", Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("Accept", "application/json");
+            request.AddParameter("grant_type", "password");
+            request.AddParameter("client_id", clientId);
+            request.AddParameter("client_secret", clientSecret);
+            request.AddParameter("username", username);
+            request.AddParameter("password", password);
+            try
+            {
+                var response = restClient.Execute(request) as RestResponse;
+                var objIncident = JsonConvert.DeserializeObject<dynamic>(response.Content);
+                // Log success
+                return objIncident.access_token.ToString();
+            }
+            catch
+            {
+                // Log exception
+                return null;
+            }
+        }
+    }
+}
---
+++ snowRequestHelper.cs
@@ -0,0 +1,159 @@
+/*
+.SYNOPSIS
+    HelperFile for making requests to the ServiceNow REST API ([Incident] table)
+.PREREQUISITES
+    Install NUGET package RestSharp v108.0.2
+    Configure classFile using .Net Core v6.0
+.DESCRIPTION
+
+    Creates a callable class called "Incident" inside the "snowRequestHelper" parent class to store incident ticket properties 
+    Creates a callable helper method called "PrepareIncidentPayload" which is used in Program.cs as a parameter in the "ExecuteRequest" method
+    Creates a callable method called "ExecuteRequest" which either sends a GET, PUT or POST request to the ServiceNow REST API, based on a switch statement
+
+.EXAMPLE
+    Call from Controller / Program using the following syntax:
+        var snowAuth = new snowAuthenticationHelper();
+        var snowRequest = new snowRequestHelper();
+        snowAuth.restClientUrl = snowRestClientUrl;
+        snowAuth.clientId = snowClientId;
+        snowAuth.clientSecret = snowClientSecret;
+        snowAuth.username = snowIntegratedUsername;
+        snowAuth.password = snowIntegratedPassword;
+        snowAuth.bearerAccessToken = snowAuth.getSnowAuthenticationToken();
+
+        var snowRequestPutJsonBuilder = new snowRequestHelper.Incident();
+        snowRequestPutJsonBuilder.WorkNotes = snowNewWorkNotes;
+
+        RestResponse preDeseralisedObjPut = snowRequest.ExecuteRequest(
+            snowAuth.restClientUrl, 
+            snowAuth.bearerAccessToken, 
+            $"api/now/table/incident/{Incident sys_id}", 
+            Method.Put, 
+            snowRequest.PrepareIncidentPayload(snowRequestPutJsonBuilder));
+
+.OUTPUTS
+    Returns a RestSharp.RestResponse class
+.NOTES
+    Created on: 14.10.2022
+    Author:     Aiden Oliver
+*/
+
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace /*InsertNamespaceHere*/.snowHelperFiles
+{
+    internal class snowRequestHelper
+    {
+
+        public class Incident
+        {
+            public string Category { get; set; }         // Example: Integration
+            public string Subcategory { get; set; }      // Example: Server
+            public string AssignedTo { get; set; }      // Example: {sys_id}
+            public string Description { get; set; }      // Example: This is a test description value
+            public string ShortDescription { get; set; }// Example: This is a test short_description value
+            public string ContactType { get; set; }     // Example: System Generated
+            public string AssignmentGroup { get; set; } // Example: 82406f631bbf8d104365a7d6b04bcb26 
+            public string CalledIentifier { get; set; }        // Example: ae1ecc361b35591022fb2069b04bcb16
+            public string WorkNotes { get; set; }       // Example: This is a test work_notes value
+        }
+
+        //public dynamic contentReturn;
+        public dynamic PrepareIncidentPayload(Incident incident)
+        {
+            Log.Information("Preparing the Json Payload");
+            dynamic jsonObject = new JObject();
+            if (incident.Category != String.Empty)
+            {
+                jsonObject.category = incident.Category;
+            }
+            if (incident.Subcategory != String.Empty)
+            {
+                jsonObject.subcategory = incident.Subcategory;
+            }
+            if (incident.AssignedTo != String.Empty)
+            {
+                jsonObject.assigned_to = incident.AssignedTo;
+            }
+            if (incident.Description != String.Empty)
+            {
+                jsonObject.description = incident.Description;
+            }
+            if (incident.ShortDescription != String.Empty)
+            {
+                jsonObject.short_description = incident.ShortDescription;
+            }
+            jsonObject.u_type = "incident";
+            if (incident.ContactType != null)
+            {
+                jsonObject.contact_type = incident.ContactType;
+            }
+            if (incident.AssignmentGroup != null)
+            {
+                jsonObject.assignment_group = incident.AssignmentGroup;
+            }
+            if (incident.CalledIentifier != null)
+            {
+                jsonObject.called_id = incident.CalledIentifier;
+            }
+            if (incident.WorkNotes != null)
+            {
+                jsonObject.work_notes = incident.WorkNotes;
+            }
+            return jsonObject;
+        }
+
+        public RestResponse? ExecuteRequest(string restClientUrl,string accessToken,string endpoint, Method method, object? body = null)
+        {
+            // CancellationTokenSource source = new CancellationTokenSource();
+            // CancellationToken token = source.Token;
+            RestClient restClient = new RestClient(restClientUrl);
+            switch (method)
+            {
+                case Method.Get:
+                    var requestGet = new RestRequest(endpoint, method);
+                    requestGet.AddHeader("Accept", "application/json");
+                    requestGet.AddHeader("Authorization", string.Format("Bearer {0}", accessToken));
+                    var responseGet = restClient.Execute(requestGet) as RestResponse;
+                    // RestResponse responseGet = RestPolicy.ExecuteTaskAsyncWithPolicy<RestResponse>(restClient, requestGet, token).Result;
+                    if (responseGet.StatusCode == HttpStatusCode.OK)
+                    {
+                        return responseGet;
+                    }
+                    break;
+                case Method.Put:
+                    RestRequest requestPut = new RestRequest(endpoint, method);
+                    requestPut.AddHeader("Accept", "application/json");
+                    requestPut.AddHeader("Authorization", string.Format("Bearer {0}", accessToken));
+                    requestPut.AddHeader("Content-Type", "application/json");
+                    requestPut.AddBody(JsonConvert.SerializeObject(body));
+                    var responsePut = restClient.Execute(requestPut) as RestResponse;
+                    // RestResponse responsePut = RestPolicy.ExecuteTaskAsyncWithPolicy<RestResponse>(restClient, requestPut, token).Result;
+                    if (responsePut.StatusCode == HttpStatusCode.OK)
+                    {
+                        return responsePut;
+                    }
+                    break;
+                case Method.Post:
+                    var requestPost = new RestRequest(endpoint, method);
+                    requestPost.AddHeader("Accept", "application/json");
+                    requestPost.AddHeader("Authorization", string.Format("Bearer {0}", accessToken));
+                    requestPost.AddHeader("Content-Type", "application/json");
+                    requestPost.AddBody(JsonConvert.SerializeObject(body));
+                    var responsePost = restClient.Execute(requestPost) as RestResponse;
+                    // RestResponse responsePost = RestPolicy.ExecuteTaskAsyncWithPolicy<RestResponse>(restClient, requestPost, token).Result;
+                    if (responsePost.StatusCode == HttpStatusCode.OK)
+                    {
+                        return responsePost;
+                    }
+                    break;
+            }
+            return null;
+
+        }
+    }
+}
